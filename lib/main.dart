a830d4fad1d47bb00a3fc766165bbc4194f1b4a4
Lisa Mae <lisa.mae@anthroware.com>
2019-12-02 14:42:49 -0500

    fix: import path

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Boring Star',
+      title: 'firefly',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
