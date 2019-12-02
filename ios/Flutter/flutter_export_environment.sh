c277c41955d75dd38f7f5715bc138c1a012d740a
Lisa Mae <lisa.mae@anthroware.com>
2019-12-02 14:26:49 -0500

    feat: create star animation

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lisamae/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lisamae/Development/FlutterProjects/firefly"
+export "FLUTTER_TARGET=/Users/lisamae/Development/FlutterProjects/firefly/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lisamae/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
