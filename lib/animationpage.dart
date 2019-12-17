2afcb015ae8e7ff15285390ea7cad260706b203a
Lisa Mae <lisa.mae@anthroware.com>
2019-12-17 17:31:45 -0500

    feat: create drawer navigation

---
+++ lib/animationpage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import "package:flare_flutter/flare_actor.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'firefly',
+theme: ThemeData(
+primarySwatch: Colors.blue,
+),
+home: MyHomePage(),
+);
+}
+}
+
+class MyHomePage extends StatefulWidget {
+@override
+_MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+@override
+Widget build(BuildContext context) {
+return new FlareActor("animations/boring_triangle.flr",
+alignment: Alignment.center,
+// fit: BoxFit.contain,
+animation: "rotate_scale_color");
+}
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,90 @@
 import 'package:flutter/material.dart';
-import "package:flare_flutter/flare_actor.dart";
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+void main()=> runApp( 
+  MaterialApp(
+    home: MyApp(),
+    title: "Navigation Drawer",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+    primarySwatch: Colors.blue,
+),
+  )
+);
+class WidgetName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'firefly',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return Container(
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
-    return new FlareActor("animations/boring_triangle.flr",
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: "rotate_scale_color");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+         backgroundColor: Colors.pink,
+      ),
+      body: Center(
+        child: Text("FireFly", style: TextStyle(
+          fontSize: 34.0,
+          color: Colors.orange[200],
+        ),)
+      ),
+      floatingActionButton: FloatingActionButton(
+
+        onPressed: (){},
+
+       
+          child: Icon(Icons.add,),
+  backgroundColor: Colors.pink,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(""),
+              accountEmail: Text(""),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFF9C4),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.pink,
+                child: Text(
+                  "L",
+                  style: TextStyle(fontSize: 40.0, color: Colors.black),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.add_to_photos),
+              title: Text("Add Items"),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text("Share with Friends"),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.rate_review),
+              title: Text("Rate and Review"),
+              onTap: (){},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
